8d47c48270c52e2c24ff370fa66f62ec12c9524d
Alex <afanasbev1@gmail.com>
2023-04-30 00:00:09 +0300

    initialcommit: finish 1st part of hometask

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Profile } from './Profile';
+import user from './Data/user.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{
+      padding: 25,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 20,
+      color: '#010101'
+    }}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
